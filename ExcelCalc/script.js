7e15f11c3ac772118a1a4f82fd2f62ecaf7b11ea
keyur32 <keyur32@hotmail.com>
2015-06-24 17:03:04 -0700

    plugging in some calculator ui

--- ExcelCalc/script.js
+++ ExcelCalc/script.js
@@ -1,26 +1,3 @@
-
-// The initialize function must be run each time a new page is loaded
-Office.initialize = function (reason) {
-	$(document).ready(function () {
-		$('#get-data-from-selection').click(getDataFromSelection);
-	});
-};
-
-// Reads data from current document selection and displays a notification
-function getDataFromSelection() {
-	console.log("getDataFromSelection called");
-	Office.context.document.getSelectedDataAsync(Office.CoercionType.Text,
-		function (result) {
-			if (result.status === Office.AsyncResultStatus.Succeeded) {
-				$('#display-data').text('The selected text is: ' + result.value);
-			} else {
-				$('#display-data').text('Error: ' + result.error.message);
-			}
-		}
-	);
-}
-
-
 (function () {
     "use strict";
 
@@ -64,3 +41,99 @@ function getDataFromSelection() {
 
 
 })();
+
+
+$(document).ready(function(e) {
+    $('#1,#2,#3,#4,#5,#6,#7,#8,#9,#0').click(function(){
+		var v = $(this).val();
+		$('#answer').val($('#answer').val() + v);	
+	});
+	$('#C').click(function(){
+		$('#answer').val('');
+		$('#operation').val('');
+		$('#operation').removeClass('activeAnswer');
+		$('#equals').attr('onclick','');
+	});
+	$('#plus').click(function(e) { 
+	
+		if($('#answer').val() == ''){
+			return false;
+			$('#equals').attr('onclick','');
+		}
+		else if ( $('#operation').attr('class') == 'activeAnswer') {
+			$('#operation').val( $('#operation').val() + $('#plus').val() );
+			$('#answer').val('');
+			$('#equals').attr('onclick','');
+		}
+		else{
+			$('#operation').val( $('#operation').val() + $('#answer').val() + $('#plus').val() );
+			$('#answer').val('');
+			$('#equals').attr('onclick','');
+		}
+    });
+	$('#subtract').click(function(e) { 
+	
+		if($('#answer').val() == ''){
+			return false;	
+			$('#equals').attr('onclick','');
+		}
+		else if ( $('#operation').attr('class') == 'activeAnswer') {
+			$('#operation').val( $('#operation').val() + $('#subtract').val() );
+			$('#answer').val('');
+			$('#equals').attr('onclick','');
+		}
+		else{
+			$('#operation').val( $('#operation').val() + $('#answer').val() + $('#subtract').val() );
+			$('#answer').val('');
+			$('#equals').attr('onclick','');
+		}
+    });
+	$('#divide').click(function(e) { 
+	
+		if($('#answer').val() == ''){
+			return false;	
+			$('#equals').attr('onclick','');
+		}
+		else if ( $('#operation').attr('class') == 'activeAnswer') {
+			$('#operation').val( $('#operation').val() + $('#divide').val() );
+			$('#answer').val('');
+			$('#equals').attr('onclick','');
+		}
+		else{
+			$('#operation').val( $('#operation').val() + $('#answer').val() + $('#divide').val() );
+			$('#answer').val('');
+			$('#equals').attr('onclick','');
+		}
+    });
+	$('#product').click(function(e) { 
+	
+		if($('#answer').val() == ''){
+			return false;	
+			$('#equals').attr('onclick','');
+		}
+		else if ( $('#operation').attr('class') == 'activeAnswer') {
+			$('#operation').val( $('#operation').val() + $('#product').val() );
+			$('#answer').val('');
+			$('#equals').attr('onclick','');
+		}
+		else{
+			$('#operation').val( $('#operation').val() + $('#answer').val() + $('#product').val() );
+			$('#answer').val('');
+			$('#equals').attr('onclick','');
+		}
+    });	
+	$('#equals').click(function(){
+		
+		if($('#equals').attr('onclick') != 'return false'){
+		
+			var a = $('#answer').val();
+			var b = $('#operation').val();
+			var c = b.concat(a);
+			$('#answer').val(eval(c));
+			$('#operation').val(eval(c));
+			$('#operation').addClass('activeAnswer');
+			$('#equals').attr('onclick','return false');
+		
+		}
+	});
+});
